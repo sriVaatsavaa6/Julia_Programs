624b5835800af3c64c762c57b6fabe6f0e0c1820
Akshay Srivatsava. B <86884558+sriVaatsavaa6@users.noreply.github.com>
2026-06-08 08:59:39 +0530

    Add matrix operations for addition and subtraction

---
+++ 9a.jl
@@ -0,0 +1,14 @@
+begin
+matrix1=[1 2 3;4 5 6;7 8 9]
+matrix2=[9 8 7;6 5 4;3 2 1]
+addition_matrix=matrix1+matrix2
+subtraction_matrix=matrix1-matrix2
+println("Matrix 1:")
+println(matrix1)
+println("Matrix 2:")
+println(matrix2)
+println("Addition Result:")
+println(addition_matrix)
+println("Subtraction Result:")
+println(subtraction_matrix)
+end
